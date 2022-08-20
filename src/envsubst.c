54671e6debe24c30fbf6e6b495a3d32022b31656
Paramtamtam <7326800+tarampampam@users.noreply.github.com>
2022-08-20 17:51:52 +0400

    wip: temporary commit

---
+++ src/envsubst.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "envsubst.h"
+
+void main() {
+	printf("foo");
+}
+
+static char *envsubst(char *str) {
+	char *result = "";
+
+	return result;
+}
---
+++ src/envsubst.h
@@ -0,0 +1,4 @@
+#ifndef GUARD_ENV_H
+#define GUARD_ENV_H
+
+#endif //GUARD_ENV_H
